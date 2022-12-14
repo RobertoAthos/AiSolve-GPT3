fe14feb7b691c2d498ab4e8275577925c46500e0
Roberto Athos <robertoathos22@gmail.com>
2022-12-14 10:22:18 -0300

    taskify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,9 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
-import { AuthProvider } from '../Context/AuthContext'
 
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
         <Component {...pageProps} />
-    </AuthProvider>
   )
 }
